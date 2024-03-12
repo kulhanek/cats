b918449c79aa1b800ec48a0580d59ac770939cd6
Petr Kulhanek <kulhanek@chemi.muni.cz>
2024-03-12 11:22:03 +0100

    top2qmfixhx added

---
+++ src/bin/amber/top/top2qmfixhx/QMFixHX.cpp
@@ -0,0 +1,158 @@
+// =============================================================================
+// CATS - Conversion and Analysis Tools
+// -----------------------------------------------------------------------------
+//    Copyright (C) 2024 Petr Kulhanek, kulhanek@chemi.muni.cz
+//    Copyright (C) 2008 Petr Kulhanek, kulhanek@enzim.hu
+//    Copyright (C) 2005 Petr Kulhanek, kulhanek@chemi.muni.cz
+//    Copyright (C) 2004 Petr Kulhanek, kulhanek@chemi.muni.cz
+//
+//     This program is free software; you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation; either version 2 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License along
+//     with this program; if not, write to the Free Software Foundation, Inc.,
+//     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+// =============================================================================
+
+#include <stdio.h>
+#include <ErrorSystem.hpp>
+#include <SmallTimeAndDate.hpp>
+#include <AmberMaskAtoms.hpp>
+#include <AmberMaskResidues.hpp>
+
+#include "QMFixHX.hpp"
+
+//==============================================================================
+//------------------------------------------------------------------------------
+//==============================================================================
+
+CQMFixHX::CQMFixHX(void)
+{
+
+}
+
+//==============================================================================
+//------------------------------------------------------------------------------
+//==============================================================================
+
+int CQMFixHX::Init(int argc,char* argv[])
+{
+    // encode program options
+    int result = Options.ParseCmdLine(argc,argv);
+
+    // should we exit or was it error?
+    if( result != SO_CONTINUE ) return(result);
+
+    // print header --------------------------------------------------------------
+    if( Options.GetOptVerbose() ) {
+        CSmallTimeAndDate dt;
+        dt.GetActualTimeAndDate();
+
+        printf("\n");
+        printf("// ==============================================================================\n");
+        printf("// top2qmfixhx (CATs utility) started at %s\n",(const char*)dt.GetSDateAndTime());
+        printf("// ==============================================================================\n");
+        printf("//\n");
+        if( Options.GetArgTopologyName() == "-" ) {
+            printf("// Topology name      : -stdin- (standard input)\n");
+        } else {
+            printf("// Topology name      : %s\n",(const char*)Options.GetArgTopologyName());
+        }
+            printf("// QM atom spec       : %s\n",(const char*)Options.GetArgQMMaskSpec());
+            printf("// No fix atom spec   : %s\n",(const char*)Options.GetArgNoFixMaskSpec());
+        printf("// ------------------------------------------------------------------------------\n");
+            printf("// Offset             : %f\n",Options.GetOptOffset());
+            printf("// Force constant     : %f\n",Options.GetOptForceConstant());
+        printf("// ------------------------------------------------------------------------------\n");
+        printf("\n");
+    }
+
+    return( result );
+}
+
+//==============================================================================
+//------------------------------------------------------------------------------
+//==============================================================================
+
+bool CQMFixHX::Run(void)
+{
+// load topology
+    if( Topology.Load(Options.GetArgTopologyName(),true) == false ) {
+        fprintf(stderr,">>> ERROR: Unable to load specified topology: %s\n",
+                (const char*)Options.GetArgTopologyName());
+        return(false);
+    }
+
+// assign topology and optionally coordinates
+
+
+// init masks
+    QMAtoms.AssignTopology(&Topology);
+    if( QMAtoms.SetMask(Options.GetArgQMMaskSpec()) == false ) {
+        fprintf(stderr,">>> ERROR: Unable to set specified QM mask!\n");
+        return(false);
+    }
+
+    NoFixAtoms.AssignTopology(&Topology);
+    if( Options.GetArgNoFixMaskSpec() != NULL ){
+        if( NoFixAtoms.SetMask(Options.GetArgNoFixMaskSpec()) == false ) {
+            fprintf(stderr,">>> ERROR: Unable to set specified NoFix mask!\n");
+            return(false);
+        }
+    }
+
+    for(int i=0; i < Topology.BondList.GetNumberOfBondsWithHydrogen(); i++){
+        CAmberBond* p_bond = Topology.BondList.GetBondWithHydrogen(i);
+        if( (QMAtoms.IsAtomSelected(p_bond->GetIB()) == false) && (QMAtoms.IsAtomSelected(p_bond->GetJB()) == false) ) continue;
+        if( (NoFixAtoms.IsAtomSelected(p_bond->GetIB()) == true) || (NoFixAtoms.IsAtomSelected(p_bond->GetJB()) == true) ) continue;
+        CAmberBondType* p_btype = Topology.BondList.GetBondType(p_bond->GetICB());
+        double len = p_btype->GetREQ();
+        CAmberAtom* p_at1 = Topology.AtomList.GetAtom(p_bond->GetIB());
+        CAmberResidue* p_res1 = p_at1->GetResidue();
+        CAmberAtom* p_at2 = Topology.AtomList.GetAtom(p_bond->GetJB());
+        CAmberResidue* p_res2 = p_at1->GetResidue();
+        printf("// :%d@%s - :%d@%s (%s/%s) REQ=%5.3f\n",p_bond->GetIB()+1,p_at1->GetName(),p_bond->GetJB()+1,p_at2->GetName(),
+                                                          p_res1->GetName(),p_res2->GetName(),len);
+        printf("RW K @%d @%d %5.3f %10.2f\n",p_bond->GetIB()+1,p_bond->GetJB()+1,len+Options.GetOptOffset(),Options.GetOptForceConstant());
+    }
+
+
+    return(true);
+}
+
+//==============================================================================
+//------------------------------------------------------------------------------
+//==============================================================================
+
+bool CQMFixHX::Finalize(void)
+{
+    if( Options.GetOptVerbose() ) {
+        CSmallTimeAndDate dt;
+        dt.GetActualTimeAndDate();
+
+        fprintf(stdout,"\n");
+        fprintf(stdout,"// ==============================================================================\n");
+        fprintf(stdout,"// %s terminated at %s\n",(const char*)Options.GetProgramName(),(const char*)dt.GetSDateAndTime());
+        fprintf(stdout,"// ==============================================================================\n");
+    }
+
+    if( Options.GetOptVerbose() || ErrorSystem.IsError() ) {
+        ErrorSystem.PrintErrors(stderr);
+        fprintf(stdout,"\n");
+        return(false);
+    }
+
+    return(true);
+}
+
+//==============================================================================
+//------------------------------------------------------------------------------
+//==============================================================================
+
---
+++ src/bin/amber/top/top2qmfixhx/QMFixHXOptions.hpp
@@ -0,0 +1,141 @@
+#ifndef QMFixHXOptionsH
+#define QMFixHXOptionsH
+// =============================================================================
+// CATS - Conversion and Analysis Tools
+// -----------------------------------------------------------------------------
+//    Copyright (C) 2024 Petr Kulhanek, kulhanek@chemi.muni.cz
+//    Copyright (C) 2008 Petr Kulhanek, kulhanek@enzim.hu
+//    Copyright (C) 2005 Petr Kulhanek, kulhanek@chemi.muni.cz
+//    Copyright (C) 2004 Petr Kulhanek, kulhanek@chemi.muni.cz
+//
+//     This program is free software; you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation; either version 2 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License along
+//     with this program; if not, write to the Free Software Foundation, Inc.,
+//     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+// =============================================================================
+
+#include <SimpleOptions.hpp>
+#include <CATsMainHeader.hpp>
+
+//------------------------------------------------------------------------------
+
+class CQMFixHXOptions : public CSimpleOptions {
+public:
+    // constructor - tune option setup
+    CQMFixHXOptions(void);
+
+// program name and description -----------------------------------------------
+    CSO_PROG_NAME_BEGIN
+    "top2qmfixhx"
+    CSO_PROG_NAME_END
+
+    CSO_PROG_DESC_BEGIN
+    "Print the names of atoms or residues selected by the application of MASK. Current implementation fully supports ambmask syntax (see AMBER manual)."
+    CSO_PROG_DESC_END
+
+    CSO_PROG_VERS_BEGIN
+    LibBuildVersion_CATs
+    CSO_PROG_VERS_END
+
+// list of all options and arguments ------------------------------------------
+    CSO_LIST_BEGIN
+    // arguments ----------------------------
+    CSO_ARG(CSmallString,TopologyName)
+    CSO_ARG(CSmallString,QMMaskSpec)
+    CSO_ARG(CSmallString,NoFixMaskSpec)
+    // options ------------------------------
+    CSO_OPT(double,Offset)
+    CSO_OPT(double,ForceConstant)
+    CSO_OPT(bool,Help)
+    CSO_OPT(bool,Version)
+    CSO_OPT(bool,Verbose)
+    CSO_LIST_END
+
+    CSO_MAP_BEGIN
+// description of arguments ---------------------------------------------------
+    CSO_MAP_ARG(CSmallString,                   /* argument type */
+                TopologyName,                          /* argument name */
+                NULL,                           /* default value */
+                true,                           /* is argument mandatory */
+                "PARM",                           /* parametr name */
+                "topology file name. If - is provided then topology is read from standard input stream.")   /* argument description */
+    //----------------------------------------------------------------------
+    CSO_MAP_ARG(CSmallString,                   /* argument type */
+                QMMaskSpec,                          /* argument name */
+                NULL,                           /* default value */
+                true,                           /* is argument mandatory */
+                "QM",                           /* parametr name */
+                "QM atom mask specification")   /* argument description */
+    //----------------------------------------------------------------------
+    CSO_MAP_ARG(CSmallString,                   /* argument type */
+                NoFixMaskSpec,                          /* argument name */
+                NULL,                           /* default value */
+                true,                           /* is argument mandatory */
+                "NoFix",                           /* parametr name */
+                "no fix atoms mask specification")   /* argument description */
+// description of options -----------------------------------------------------
+    CSO_MAP_OPT(double,                           /* option type */
+                Offset,                        /* option name */
+                0.0,                          /* default value */
+                false,                          /* is option mandatory */
+                'o',                           /* short option name */
+                "offset",                      /* long option name */
+                NULL,                           /* parametr name */
+                "offset to equlibrium atoms")   /* option description */
+    //----------------------------------------------------------------------
+    CSO_MAP_OPT(double,                           /* option type */
+                ForceConstant,                        /* option name */
+                100.0,                          /* default value */
+                false,                          /* is option mandatory */
+                'f',                           /* short option name */
+                "fc",                      /* long option name */
+                NULL,                           /* parametr name */
+                "force constant")   /* option description */
+    //----------------------------------------------------------------------
+    CSO_MAP_OPT(bool,                           /* option type */
+                Verbose,                        /* option name */
+                false,                          /* default value */
+                false,                          /* is option mandatory */
+                'v',                           /* short option name */
+                "verbose",                      /* long option name */
+                NULL,                           /* parametr name */
+                "increase output verbosity")   /* option description */
+    //----------------------------------------------------------------------
+    CSO_MAP_OPT(bool,                           /* option type */
+                Version,                        /* option name */
+                false,                          /* default value */
+                false,                          /* is option mandatory */
+                '\0',                           /* short option name */
+                "version",                      /* long option name */
+                NULL,                           /* parametr name */
+                "output version information and exit")   /* option description */
+    //----------------------------------------------------------------------
+    CSO_MAP_OPT(bool,                           /* option type */
+                Help,                        /* option name */
+                false,                          /* default value */
+                false,                          /* is option mandatory */
+                'h',                           /* short option name */
+                "help",                      /* long option name */
+                NULL,                           /* parametr name */
+                "display this help and exit")   /* option description */
+    CSO_MAP_END
+
+// final operation with options ------------------------------------------------
+private:
+    virtual int CheckOptions(void);
+    virtual int FinalizeOptions(void);
+    virtual int CheckArguments(void);
+};
+
+//------------------------------------------------------------------------------
+
+#endif
